da5f84ce9a3056590b2b123884e2a0c885491fea
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-07-03 23:49:59 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+title = 'LET ME KNOW'
+maturity = 12
+InterestRate1 = 1.47
+InterestRate2 = 1.46
+InterestRate3 = 1.47
+
+print(title)
+print(type(title))
+print(type(maturity))
+print(type(InterestRate1))
+
+message = "Welcome"
+CustomerName = "Engin"
+CustomerSurname = "Demiroğ"
+ResultMessage = message+" "+CustomerName+" "+CustomerSurname
+
+print(ResultMessage)
+
+number1 = 10
+number2 = 20
+print(number1 + number2)
