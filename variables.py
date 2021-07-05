520543b6f5a87353c32671fc17edd7b183346124
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-07-05 14:35:13 +0300

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,11 @@
+USDYesterday = 7.85
+USDToday = 7.85
+
+if USDYesterday<USDToday:
+  print("Increase")
+
+if USDYesterday>USDToday:
+  print("Decrease")
+
+if USDToday==USDYesterday:
+  print("Equal")
